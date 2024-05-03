f4b3106b3a96a826972507bf7c8a2afa72b9b151
Daniel Silva <devmaquina.ds@gmail.com>
2024-05-03 09:18:55 -0300

    Ajustes

--- wp-content/themes/plebicito/header.php
+++ wp-content/themes/plebicito/header.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
+    <title><?php bloginfo('name'); ?></title>
 </head>
 <body <?php body_class(); ?>>
     <header>
